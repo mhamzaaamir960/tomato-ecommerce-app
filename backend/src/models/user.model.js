5bfeb85c6a6dec9559e2454dadbaa5badf57a809
mhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-12-09 00:30:43 +0500

    added user schema model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+export const User = mongoose.model("User", userSchema);
